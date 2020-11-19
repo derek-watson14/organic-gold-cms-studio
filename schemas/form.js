b5bfdc818dea83a862ad36e9c17807d4b3f21fb9
Derek Watson <derek.watson92@gmail.com>
2020-11-19 13:27:52 -0800

    changed forms so only fields are editable

--- schemas/form.js
+++ schemas/form.js
@@ -16,28 +16,13 @@ export default {
       validation: Rule => Rule.required(),
     },
     {
-      name: 'formLabelList',
-      title: 'Form Label List',
+      name: 'fieldLabels',
+      title: 'Fields',
       type: 'array',
       of: [{
-        name: 'formFieldLabel',
-        title: 'Form Field Label',
-        type: 'object',
-        fields: [
-          {
-            name: 'fieldName',
-            title: 'Field name',
-            type: 'string',
-            validation: Rule => Rule.required(),
-          },
-          {
-            name: 'fieldLabelText',
-            title: 'Field Label',
-            type: 'string',
-            validation: Rule => Rule.required(),
-          },
-
-        ]
+        name: 'label',
+        title: 'Field label',
+        type: 'string',
       }],
     },
   ],
