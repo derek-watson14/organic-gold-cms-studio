2f5941a50165820357ced47082d028726a60ef13
Derek Watson <derek.watson92@gmail.com>
2020-11-19 13:31:33 -0800

    edited titles for forms

--- schemas/form.js
+++ schemas/form.js
@@ -17,11 +17,11 @@ export default {
     },
     {
       name: 'fieldLabels',
-      title: 'Fields',
+      title: 'Field labels',
       type: 'array',
       of: [{
         name: 'label',
-        title: 'Field label',
+        title: 'Label',
         type: 'string',
       }],
     },
